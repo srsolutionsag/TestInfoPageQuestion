62c1d66cfeeac206a7fbc3ca5eee0d639d9eeb29
Fabian Schmid <fs@studer-raimann.ch>
2014-12-08 09:34:02 +0100

    Prepared release

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'infopageta';
-$version = '0.0.6';
-$ilias_min_version = '4.3.0';
-$ilias_max_version = '4.5.999';
+$version = '1.0.0';
+$ilias_min_version = '4.4.0';
+$ilias_max_version = '4.4.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
