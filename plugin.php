abe7f8b5159d72f05857814c85ee28223d6a7865
Benjamin Seglias <bs@studer-raimann.ch>
2018-04-30 14:36:14 +0200

    adopted ILIAS min and max version, increased plugin version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'infopageta';
-$version = '1.2.1';
-$ilias_min_version = '5.2.0';
-$ilias_max_version = '5.2.999';
+$version = '1.3.1';
+$ilias_min_version = '5.3.0';
+$ilias_max_version = '5.3.999';
 $responsible = 'PHZH';
 $responsible_mail = 'pascal.schmitt@phzh.ch';
