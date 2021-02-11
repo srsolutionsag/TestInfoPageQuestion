f7c5f8f7c90537cd3a2440e093c5714a00156a91
Thibeau Fuhrer <thf@studer-raimann.ch>
2021-02-11 15:44:43 +0100

    increased plugin and max-ilias version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'infopageta';
-$version = '1.3.2';
+$version = '1.3.3';
 $ilias_min_version = '5.4.0';
-$ilias_max_version = '5.4.999';
+$ilias_max_version = '6.9.999';
 $responsible = 'PHZH';
 $responsible_mail = 'pascal.schmitt@phzh.ch';
